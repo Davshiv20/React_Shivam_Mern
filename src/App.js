57d7254fb1d8e7599496c2367cc0c578bd89c707
Shivam <shivamdave2903@gmail.com>
2023-10-12 22:31:36 +0530

    Adding GitHub Pages

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1><p>Tasks By Shivam Dave 21BCB0107</p></h1>
+      <table>
+        <tr>
+          <th>Task</th>
+        
+          <th className='th1'>Vercel Deployment Link</th>
+        </tr>
+        <tr>
+          <td>Task 1</td>
+          <td className='th1'><a href="https://task1-ethnus.vercel.app" target='_blank' >Live</a></td>
+        </tr>
+        <tr>
+          <td>Task 2</td>
+          <td className='th1'><a href="https://task2-ethnus.vercel.app" target='_blank' >Live</a></td>
+        </tr>
+        <tr>
+          <td>Task 3</td>
+          <td className='th1'><a href="https://task3-ethnus.vercel.app" target='_blank' >Live</a></td>
+        </tr>
+        <tr>
+          <td>Task 4</td>
+          <td className='th1'><a href="https://task4-ethnus.vercel.app" target='_blank' >Live</a></td>
+        </tr>
+        <tr>
+          <td>Task 5</td>
+          <td className='th1'><a href="https://task5-ethnus.vercel.app" target='_blank' >Live</a></td>
+        </tr>
+       
+      </table>
+    </div>
+  );
+}
+
+export default App;
